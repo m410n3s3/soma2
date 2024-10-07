7839494e599163e560aafcb39bc9a0b368373909
m410n3s3 <gustavolaurentino2009@gmail.com>
2024-10-07 13:16:54 -0300

    ...

--- exerSalarioBonus.py
+++ exerSalarioBonus.py
@@ -12,3 +12,33 @@ if __name__=="__main__":
     Nome, Comissao, PagamentoEsperado = Comissaosalario()
     strg = "{0} obteve R$: {1:.2f} de comissão e vai receber: {2:.2f}".format(Nome,Comissao,PagamentoEsperado)
     print(strg)
+
+
+
+
+
+
+
+#     import math as m
+
+# def eq2grau(a,b,c):
+#     delta = m.pow(b,2)-4*a*c
+#     if (delta>0):
+
+#         x1= (-b-pow(delta,0.5))/(2*a)
+#         x2= (-b+pow(delta,0.5))/(2*a)
+
+#         return x1 , x2
+#     elif (delta==0):
+#         print("")
+
+
+
+
+
+
+
+
+# if __name__=="__main__":
+#     x1, x2 = eq2grau(1,4,3)
+#     print(f"as raízes são {x1} e {x2}")
